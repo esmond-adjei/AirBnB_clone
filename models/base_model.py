9e2833dacd835947049cce1dc5cb63107c00498b
esmond-adjei <xmongram@gmail.com>
2023-05-12 11:13:18 +0000

    modified some files

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,17 @@ from datetime import datetime
 
 class BaseModel:
 	"""Parent/base class. All other classes inherits from here."""
-	def __init__(self):
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
+	def __init__(self, *args, **kwargs):
+		if kwargs:
+			del kwargs['__class__']
+			for k, v in kwargs.items():
+				if k == 'created_at' or k == 'updated_at':
+					v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+				setattr(self, k, v)
+		else:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
 
 	def __str__(self):
 		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -24,7 +31,7 @@ class BaseModel:
 	def to_dict(self):
 		cls_dict = {'__class__' : self.__class__.__name__}
 		cls_dict.update({k: v.isoformat() \
-			     if isinstance(v, datetime) \
-			     else v for k, v in self.__dict__.items()})
+				 if isinstance(v, datetime) \
+				 else v for k, v in self.__dict__.items()})
 		return cls_dict
 	
