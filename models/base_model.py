257502304dc6649b7b4e3a4bcd18d6fb9d893fa2
esmond-adjei <xmongram@gmail.com>
2023-05-12 14:06:18 +0000

    modified some files

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ Implementation of the parent class (BasedModel).
 
 import uuid
 from datetime import datetime
+import storage
 
 
 class BaseModel:
@@ -21,12 +22,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save(self)
 
     def to_dict(self):
         cls_dict = {'__class__': self.__class__.__name__}
---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+"""Definition of file storage class"""
+
+
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Serializes instances to a JSON file 
+       and deserializes JSON file to instances."""
+    
+    __file_path = 'file_object.json'
+    __objects = {}
+
+    all_classes = {"BaseModel": BaseModel}
+
+    def all(self):
+        """returns the dictionary `__objects`"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in objects the object with key `<obj name>.id`"""
+        if obj is not None:
+            obj_id = obj.__class__.__name__ + "." + obj.id
+            self.__objects[obj_id] = obj
+
+    def save(self):
+        f"""serializes objects to JSON file (path: {__file_path})"""
+        json_objs = {}
+        # convert object to dictionary before saving to file
+        for obj_id, obj in self.__objects.items():
+            json_objs[obj_id] = obj.to_dict()
+        with open(__file_path, 'w') as f:
+            json.dump(json_objs, f)
+
+    def reload(self):
+        try:
+            f"""deserializes the JSON file (path: {__file_path})"""
+            with open(__file_path, 'r') as f:
+                json_obj = json.load(self.__objects, f)
+
+            # converting from dictionary objects to class objects.
+            # this is done by selecting the class type from all_classes dictionary
+            for obj_id, obj_dict in json_obj.items():
+                self.__objects[obj_id] = all_classes[obj_dict["__class__"]](**obj_cit)
+        except Exception as err:
+            print("Error occured while opening file", err)
+
