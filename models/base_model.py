2059fbf4193dca33f28e122f0efcad4155f2c469
esmond-adjei <xmongram@gmail.com>
2023-05-12 10:15:27 +0000

    modified some files

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,27 @@ Implementation of the parent class (BasedModel).
 """
 
 
+import uuid
+from datetime import datetime
+
+
 class BaseModel:
     """Parent/base class. All other classes inherits from here."""
+	def __init__(self):
+        	self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
+	
+	def __str__(self):
+		retturn f"{self.__class__.__name__} {(self.id)} {self.__dict__}"
 
-    def __init__(self):
-        pass
+	def save(self):
+		self.updated_at = datetime.now()
+
+	def to_dict(self):
+		cls_dict = {__class__ : self.__class__.__name__}
+		cls_dict.update({k: v.isoformat() \
+			     if isinstance(v, datetime) \
+			     else v for k, v in self.__dict__.items()})
+		return cls_dict
+		
