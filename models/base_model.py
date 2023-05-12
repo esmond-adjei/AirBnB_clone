9bdd9684867c06bd4912c9ffe5f5b8a88e3d4d3e
esmond-adjei <xmongram@gmail.com>
2023-05-12 04:22:57 -0700

    modified some files

--- models/base_model.py
+++ models/base_model.py
@@ -17,10 +17,10 @@ class BaseModel:
                 if k == 'created_at' or k == 'updated_at':
                     v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, k, v)
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
