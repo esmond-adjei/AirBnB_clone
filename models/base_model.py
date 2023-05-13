fd021acfef54c30e3eaea040f23c0b3697d5a68f
esmond-adjei <xmongram@gmail.com>
2023-05-13 15:43:50 -0700

    modified some files

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ Implementation of the parent class (BasedModel).
 
 import uuid
 from datetime import datetime
-import models
+# import models
 
 
 class BaseModel:
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 """Definition of file storage class"""
 
 
-from models.base_model import BaseModel
+from ..base_model import BaseModel
 
 
 class FileStorage:
