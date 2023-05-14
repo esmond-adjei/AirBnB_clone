9d635ef26b477081fb6c4396a230b6ce1ccee75a
esmond-adjei <xmongram@gmail.com>
2023-05-14 05:17:56 -0700

    modified some files

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,2 +1,35 @@
 #!/usr/bin/python3
 """ Tests for the base class model """
+
+import unittest
+
+import uuid
+from datetime import datetime
+import models
+
+BaseModel = models.base_model.BaseModel
+
+
+class TestBaseModel:
+    """
+    Tests to run on Base Model.
+    """
+    def test_instantiation(self):
+        """Tests for object instantiation"""
+        model = BaseModel()
+        self.assertIs(type(model), BaseModel)
+        model.name = 'example'
+        model.age = 88
+        attrs_types = {
+            "id": str,
+            "created_at": datetime,
+            "updated_at": datetime,
+            "name": str,
+            "number": int
+        }
+        for attr, typ in attrs_types.items():
+            with self.subTest(attr=attr, typ=typ):
+                self.assertIn(attr, model.__dict__)
+                self.assertIs(type(model.__dict__[attr]), typ)
+        self.assertEqual(model.name, "example")
+        self.assertEqual(model.number, 88)
