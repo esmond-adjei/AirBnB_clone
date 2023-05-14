47022fbfebd719c034df41bf81c4760f6f23576c
esmond-adjei <xmongram@gmail.com>
2023-05-13 17:34:46 -0700

    modified some files

--- console.py
+++ console.py
@@ -3,6 +3,10 @@
 
 
 import cmd
+from models.base_model import BaseModel
+
+all_models = {"BaseModel": BaseModel}
+new_model = ''
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,8 +24,33 @@ class HBNBCommand(cmd.Cmd):
         """Overlook empty lines"""
         return False
 
-    def do_create(self):
-        
+    def do_create(self, line):
+        """Create an instance of BaseModel"""
+        args = line.split(" ")
+        if not args[0]:
+            print("** class name missing **")
+        elif args[0] not in all_models.keys():
+            print("** class doesn't exist **")
+        else:
+            new_model = all_models[args[0]]()
+            print(new_model.id)
+
+    def do_show(self, line):
+        """
+        Prints the string reprensentation of an instance based
+        on the calss name and id.
+        """
+        args = line.split(" ")
+        if not args[0]:
+            print("** class name missing **")
+        elif args[0] not in all_models.keys():
+            print("** class doesn't exist **")
+        elif not args[1]:
+            print("** instance id missing **")
+        elif new_model.id != args[1]:
+            print("** no instance found **")
+        else:
+            print(new_model.id)
 
 
 if __name__ == "__main__":
