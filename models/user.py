abcc69cf77f49188ed9dbcee667dca3ad4eee376
ProvLeon <providence.leonard@gmail.com>
2023-05-15 17:00:07 +0100

    updated user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """Implementation of user class"""
 
+from models.base_model import BaseModel
 
 class User:
     """User class implementation"""
 
     def __init__(self):
-        pass
+        super().__init__(*args, **kwargs)
+        self.username = ""
+        self.password = ""
+        self.email = ""
