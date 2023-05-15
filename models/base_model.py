d27648400abb27add868e98c788e86c4000b21ee
ProvLeon <providence.leonard@gmail.com>
2023-05-15 17:16:35 +0100

    updated BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,43 @@
+#!/usr/bin/python3
+"""
+Implementation of the parent class (BasedModel).
+"""
+import os
+import sys
 import uuid
 from datetime import datetime
+import models
+
+
+sys.path.append('..')
 
 
 class BaseModel:
-    """BaseModel class for AirBnB objects."""
+    """Parent/base class. All other classes inherits from here."""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            del kwargs['__class__']
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.__dict__
-        )
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        cls_dict = {'__class__': self.__class__.__name__}
+        cls_dict.update({k: v.isoformat()
+                        if isinstance(v, datetime)
+                        else v for k, v in self.__dict__.items()})
+        return cls_dict
