e9e2aec7e8a0525a6f3b72bfbafa4e1cd14e3274
ProvLeon <providence.leonard@gmail.com>
2023-05-16 00:10:59 +0100

    Updated file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
     """Serializes instances to a JSON file
     and deserializes JSON file to instances."""
 
-    __file_path = 'file_object.json'
+    __file_path = 'file.json'
     __objects = {}
 
     def new(self, obj):
@@ -19,21 +19,9 @@ class FileStorage:
             obj_id = obj.__class__.__name__ + "." + obj.id
             self.__objects[obj_id] = obj
 
-    def all(self, model_type=None):
+    def all(self):
         """Returns the dictionary `__objects`"""
-        if model_type:
-            model_cat = list(filter(lambda obj:
-                                    str(obj.__class__.__name__) == model_type,
-                                    self.__objects.values()))
-            return [str(m) for m in model_cat]
         return self.__objects
-
-    def delete(self, model_id):
-        """Deletes a model from data `__objects`"""
-        if self.__objects.get(model_id, 0):
-            del self.__objects[model_id]
-        else:
-            print("Model not found")
 
     def save(self):
         """Serializes objects to JSON file (path: {self.__file_path})"""
@@ -42,8 +30,8 @@ class FileStorage:
         for obj_id, obj in self.__objects.items():
             json_objs[obj_id] = obj.to_dict()
         try:
-            with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
-                json.dump(json_objs, file)
+            with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+                json.dump(json_objs, f)
         except FileNotFoundError as err:
             print(f"Error occurred while opening file: {err}")
 
@@ -52,9 +40,8 @@ class FileStorage:
         all_classes = {"BaseModel": BaseModel}
         if os.path.isfile(FileStorage.__file_path):
             try:
-                with open(FileStorage.__file_path,
-                          'r', encoding='utf-8') as file:
-                    json_obj = json.load(file)
+                with open(FileStorage.__file_path, 'r', encoding='utf-8') as f:
+                    json_obj = json.load(f)
 
                 # Converting from dictionary objects to class objects.
                 # This is done by selecting the class type
@@ -62,7 +49,7 @@ class FileStorage:
                 for obj_id, obj_dict in json_obj.items():
                     class_type = obj_dict["__class__"]
                     FileStorage.__objects[obj_id] = all_classes[
-                            class_type](**obj_dict)
+                        class_type](**obj_dict)
             except json.JSONDecodeError as err:
                 print(f"Error occurred while decoding JSON: {err}")
             except KeyError as err:
