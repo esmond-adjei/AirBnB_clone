bd55d4a26d1ff27688c81fe4a87b0ed708f14536
ProvLeon <providence.leonard@gmail.com>
2023-05-16 00:55:56 +0100

    updated base model file

--- models/base_model.py
+++ models/base_model.py
@@ -1,46 +1,46 @@
 #!/usr/bin/python3
-"""Module for base model class"""
-
+"""Implementation of the parent class (BasedModel)."""
+import sys
 import uuid
 from datetime import datetime
-from models import storage
+import models
+
+sys.path.append('..')
 
 
 class BaseModel:
-    """Base model class"""
+    """Parent/base class. All other classes inherits from here."""
 
     def __init__(self, *_args, **kwargs):
-        """Constructor for base model class"""
         if kwargs:
-            self.__set_attributes(kwargs)
+            del kwargs['__class__']
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
-    def __set_attributes(self, attributes):
-        """Sets the attributes of the model"""
-        if '__class__' in attributes:
-            del attributes['__class__']
-        if 'created_at' in attributes:
-            attributes['created_at'] = datetime.strptime(
-                attributes['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-        if 'updated_at' in attributes:
-            attributes['updated_at'] = datetime.strptime(
-                attributes['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-        for key, value in attributes.items():
-            setattr(self, key, value)
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the updated_at attribute and saves the model to file"""
+        """A method to save attributes of an instance
+        """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of the model"""
-        attributes = self.__dict__.copy()
-        attributes['__class__'] = self.__class__.__name__
-        attributes['created_at'] = self.created_at.isoformat()
-        attributes['updated_at'] = self.updated_at.isoformat()
-        return attributes
+        """hadles the key-paired values to dictionary
+
+        Returns:
+            dict: return dictionary
+        """
+        cls_dict = {'__class__': self.__class__.__name__}
+        cls_dict.update({k: v.isoformat()
+                        if isinstance(v, datetime)
+                        else v for k, v in self.__dict__.items()})
+        return cls_dict
