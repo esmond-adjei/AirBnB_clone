54747f7ff1f06e0e4dc5f914ceb3393167e4218e
ProvLeon <providence.leonard@gmail.com>
2023-05-16 09:11:08 +0100

    Updated User class

--- models/user.py
+++ models/user.py
@@ -4,11 +4,9 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class that inherits from BaseModel"""
-    def __init__(self, *args, **kwargs):
-        """Initialize User attributes"""
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    """Implementation of the Class for managing user objects"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
