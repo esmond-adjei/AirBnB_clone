9480cd005a117ff70428e6a9fd262ac75cfedec6
ProvLeon <providence.leonard@gmail.com>
2023-05-16 02:02:31 +0100

    created additional model files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """Module for FileStorage class."""
-# import datetime
-from models.base_model import BaseModel
-from models.user import User
+import datetime
 import json
 import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -31,19 +36,13 @@ class FileStorage:
     def classes(self):
         """Returns a dictionary of valid classes and their references"""
 
-        # from models.state import State
-        # from models.city import City
-        # from models.amenity import Amenity
-        # from models.place import Place
-        # from models.review import Review
-
         classes = {"BaseModel": BaseModel,
                    "User": User,
-                   #    "State": State,
-                   #    "City": City,
-                   #    "Amenity": Amenity,
-                   #    "Place": Place,
-                   #    "Review": Review
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review
                    }
         return classes
 
@@ -58,40 +57,40 @@ class FileStorage:
             # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
-    # def attributes(self):
-    #     """Returns the valid attributes and their types for classname"""
-    #     attributes = {
-    #             "BaseModel":
-    #             {"id": str,
-    #              "created_at": datetime.datetime,
-    #              "updated_at": datetime.datetime},
-    #             "User":
-    #             {"email": str,
-    #              "password": str,
-    #              "first_name": str,
-    #              "last_name": str},
-    #             "State":
-    #             {"name": str},
-    #             "City":
-    #             {"state_id": str,
-    #              "name": str},
-    #             "Amenity":
-    #             {"name": str},
-    #             "Place":
-    #             {"city_id": str,
-    #              "user_id": str,
-    #              "name": str,
-    #              "description": str,
-    #              "number_rooms": int,
-    #              "number_bathrooms": int,
-    #              "max_guest": int,
-    #              "price_by_night": int,
-    #              "latitude": float,
-    #              "longitude": float,
-    #              "amenity_ids": list},
-    #             "Review":
-    #             {"place_id": str,
-    #              "user_id": str,
-    #              "text": str}
-    #             }
-    #     return attributes
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+                "BaseModel":
+                {"id": str,
+                 "created_at": datetime.datetime,
+                 "updated_at": datetime.datetime},
+                "User":
+                {"email": str,
+                 "password": str,
+                 "first_name": str,
+                 "last_name": str},
+                "State":
+                {"name": str},
+                "City":
+                {"state_id": str,
+                 "name": str},
+                "Amenity":
+                {"name": str},
+                "Place":
+                {"city_id": str,
+                 "user_id": str,
+                 "name": str,
+                 "description": str,
+                 "number_rooms": int,
+                 "number_bathrooms": int,
+                 "max_guest": int,
+                 "price_by_night": int,
+                 "latitude": float,
+                 "longitude": float,
+                 "amenity_ids": list},
+                "Review":
+                {"place_id": str,
+                 "user_id": str,
+                 "text": str}
+                }
+        return attributes
--- models/user.py
+++ models/user.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Implementation of user class"""
-
+"""This module creates a User class"""
 from models.base_model import BaseModel
 
 
-class User:
-    """User class implementation"""
+class User(BaseModel):
+    """Implementation of the Class for managing user objects"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.username = ""
-        self.password = ""
-        self.email = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
