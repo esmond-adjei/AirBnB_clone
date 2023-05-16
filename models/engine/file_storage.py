d4bfa5cd2b1c072f47feb0c14fa50149490d0a2f
ProvLeon <providence.leonard@gmail.com>
2023-05-16 01:51:52 +0100

    Updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,61 +1,97 @@
 #!/usr/bin/python3
-"""Definition of file storage class"""
-
-import json
+"""Module for FileStorage class."""
+# import datetime
 from models.base_model import BaseModel
+from models.user import User
+import json
+import os
 
 
 class FileStorage:
-    """Serializes instances to a JSON file
-    and deserializes JSON file to instances."""
 
-    __file_path = 'file_object.json'
+    """Class for storing and retrieving data"""
+    __file_path = "file.json"
     __objects = {}
 
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
     def new(self, obj):
-        """sets in objects the object with key `<obj name>.id`"""
-        if obj is not None:
-            obj_id = obj.__class__.__name__ + "." + obj.id
-            self.__objects[obj_id] = obj
-
-    def all(self, model_type=None):
-        """returns the dictionary `__objects`"""
-        if model_type:
-            model_cat = list(filter(lambda obj:
-                                    str(obj.__class__.__name__) == model_type,
-                                    self.__objects.values()))
-            return [str(m) for m in model_cat]
-        return self.__objects
-
-    def delete(self, model_id):
-        """deletes a model from data `__objects`"""
-        if self.__objects.get(model_id, 0):
-            del self.__objects[model_id]
-        else:
-            print("Model not found")  # xx
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """serializes objects to JSON file (path: {self.__file_path})"""
-        json_objs = {}
-        # convert object to dictionary before saving to file
-        for obj_id, obj in self.__objects.items():
-            json_objs[obj_id] = obj.to_dict()
-        with open(self.__file_path, 'w', encoding='utf-8') as _f:
-            json.dump(json_objs, _f)
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+
+        # from models.state import State
+        # from models.city import City
+        # from models.amenity import Amenity
+        # from models.place import Place
+        # from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   #    "State": State,
+                   #    "City": City,
+                   #    "Amenity": Amenity,
+                   #    "Place": Place,
+                   #    "Review": Review
+                   }
+        return classes
 
     def reload(self):
-        """deserializes the JSON file (path: {self.__file_path})"""
-        all_classes = {"BaseModel": BaseModel}
+        """Reloads the stored objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
 
-        try:
-            with open(self.__file_path, 'r', encoding='utf-8') as _f:
-                json_obj = json.load(_f)
-
-            # converting from dictionary objects to class objects.
-            # this is done by selecting the class type
-            # from all_classes dictionary
-            for obj_id, obj_dict in json_obj.items():
-                class_type = obj_dict["__class__"]
-                self.__objects[obj_id] = all_classes[class_type](**obj_dict)
-        except Exception as err:
-            print(f"Error occurred while opening file: {err}")
+    # def attributes(self):
+    #     """Returns the valid attributes and their types for classname"""
+    #     attributes = {
+    #             "BaseModel":
+    #             {"id": str,
+    #              "created_at": datetime.datetime,
+    #              "updated_at": datetime.datetime},
+    #             "User":
+    #             {"email": str,
+    #              "password": str,
+    #              "first_name": str,
+    #              "last_name": str},
+    #             "State":
+    #             {"name": str},
+    #             "City":
+    #             {"state_id": str,
+    #              "name": str},
+    #             "Amenity":
+    #             {"name": str},
+    #             "Place":
+    #             {"city_id": str,
+    #              "user_id": str,
+    #              "name": str,
+    #              "description": str,
+    #              "number_rooms": int,
+    #              "number_bathrooms": int,
+    #              "max_guest": int,
+    #              "price_by_night": int,
+    #              "latitude": float,
+    #              "longitude": float,
+    #              "amenity_ids": list},
+    #             "Review":
+    #             {"place_id": str,
+    #              "user_id": str,
+    #              "text": str}
+    #             }
+    #     return attributes
