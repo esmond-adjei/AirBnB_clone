1082722bcd116d30c79dc3aa0b543c037f8871b3
ProvLeon <providence.leonard@gmail.com>
2023-05-16 01:39:31 +0100

    Updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
     """Serializes instances to a JSON file
     and deserializes JSON file to instances."""
 
-    __file_path = 'file_object.json'
+    __file_path = 'file.json'
     __objects = {}
 
     def new(self, obj):
